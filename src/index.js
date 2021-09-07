8e3c5a080e874e26b6e252dc9305dda84409d0f0
Jonathan Correa Paiva <jona.correapc@gmail.com>
2021-09-07 09:55:49 -0300

    Included bot router

--- src/index.js
+++ src/index.js
@@ -4,16 +4,24 @@ if (dotenv.error) {
   throw dotenv.error;
 }
 
-const express = require("express");
-const { format } = require("winston");
 const logger = require("./helpers/logger");
+
+const express = require("express");
 const app = express();
+const helmet = require("helmet");
 const port = process.env.APP_PORT;
+
+const router = require("./routes");
+
+
+app.use(helmet());
+
+app.use("/bot", router.bot);
 
 app.get("/", (req,res) => {
   res.send("Express app");
 });
 
 app.listen(port, () => {
-  logger.log("info", `Example app listening at http://localhost:${port}`);
+  logger.log("info", `gChatPromAlert listening in port: ${port}`);
 });
