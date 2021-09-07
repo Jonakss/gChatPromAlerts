4f567fdc74611626a35d7d2db52dfbb71b47b09f
Jonathan Correa Paiva <jona.correapc@gmail.com>
2021-09-07 09:55:13 -0300

    fixed route, Added scafold in bot

--- src/routes/bot.js
+++ src/routes/bot.js
@@ -1,14 +1,65 @@
-// var express = require('express');
-var router = express.Router();
-router.use(express.json);
+// wiki.js - Wiki route module.
 
-//ADDED_TO_SPACE
-//MESSAGE
-//REMOVED_FROM_SPACE
+var express = require('express');
+var router = express.Router();
+
+// Home page route.
+router.get('/', function (req, res) {
+  res.send('Whelcome to /bot gChatPromAlerts');
+})
+
+// About page route.
+router.get('/about', function (req, res) {
+  res.send('About this wiki');
+})
+
+/// From Google Chat ///
+var fireEvent = (event) => {
+    // ON_ADDED_TO_SPACE
+    onAddedToSpace = (body) => {
+        console.log("onAddedToSpace");
+        return "onAddedToSpace";
+    };
+    // ON_REMOVED_FROM_SPACE
+    onRemovedFromSpace = (body) => {
+
+    };
+    // MESSAGE
+    onMessage = (body) => {
+        var execCommand = (command) => { 
+            let commands = {
+        
+            };
+            let notACommand = () => {
+                
+            };
+        
+            return command? commands[command]() : notACommand();
+        };
+        
+    };
+
+    notAEvent = () => {
+        throw 'Event type undefined or not supported';
+    };
+
+    var events = {
+        ON_ADDED_TO_SPACE: onAddedToSpace,
+        ON_REMOVED_FROM_SPACE: onRemovedFromSpace,
+        MESSAGE: onMessage
+    };
+
+    return event? events[event]() : notAEvent();
+};
+
+
 router.post("/", (req, res) => {
-    res.send("From /bot");
+    getEvent(req.body.type);
 });
-router.get("/", (req, res) => {
-    res.send("From /bot");
+
+router.get("/test/:type", (req, res) => {
+    var a = fireEvent(req.params.type);
+    res.send(a);
 });
+
 module.exports = router;
