7412c243e908c9fb2cc9579e14c33da4d3974bd0
Jonathan Correa Paiva <jona.correapc@gmail.com>
2021-09-07 09:59:24 -0300

    Cleaned structure

--- routes/webgui.js
+++
@@ -1,9 +0,0 @@
-// var express = require('express');
-var router = express.Router();
-router.use(express.json);
-
-router.get("/", (req, res) => {
-    res.send("From /");
-});
-
-module.exports = router;
