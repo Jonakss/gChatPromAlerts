a9d61902c6155f47960704cbc7f6207b15f26af2
Jonathan Correa Paiva <jona.correapc@gmail.com>
2021-09-08 09:45:20 -0300

    googleChatApiRefactor

--- src/helpers/googleChatApi.js
+++ src/helpers/googleChatApi.js
@@ -1,100 +1,70 @@
 const { google } = require('googleapis');
 const gchat = google.chat('v1');
+const logger = require('./logger');
 
 const _auth = new google.auth.GoogleAuth({
     // Scopes can be specified either as an array or as a single, space-delimited string.
     scopes: ['https://www.googleapis.com/auth/chat.bot'],
 });
 
+async function updateMessage(space, thread ,message) {
+    google.options({auth: _auth});
+    const res = await gchat.spaces.messages.send({
+        name: `spaces/${space}/messages/${thread}.${message}`,
+        updateMask: 'text',
+        requestBody:{},
+    });
+    return res;
+};
 
-function postMessage(count) {
-    return new Promise(function(resolve, reject) {
-        getJWT().then(function(token) {
-            unirest.post('https://chat.googleapis.com/v1/spaces/#####/messages/')
-                .headers({
-                    "Content-Type": "application/json",
-                    "Authorization": "Bearer " + token,
-            "updateMask":"text"
-                })
-                .send(JSON.stringify({
-                    'text': 'Hello! This is message number ' + count,
-                }))
-                .end(function(res) {
-                console.log(res);
-                    resolve();
-                });
-        }).catch(function(err) {
-            reject(err);
-        });
+async function sendMessage(space, thread, messageId, message) {
+    
+    const auth = new google.auth.GoogleAuth({
+    // Scopes can be specified either as an array or as a single, space-delimited string.
+        scopes: ['https://www.googleapis.com/auth/chat.bot'],
     });
-  }
-  
-  async function updateMessage(space, thread ,message) {
+
+    // Acquire an auth client, and bind it to all future calls
     const authClient = await auth.getClient();
     google.options({auth: authClient});
-  const res = await gchat.spaces.messages.update({
-      name: `spaces/${space}/messages/${thread}.${message}`,
-      updateMask: 'text',
-      requestBody:{}
-  );
-  console.log(res.data);
-  }
+    let res = await gchat.spaces.messages.create({
+        name: `spaces/${space}`,
+        requestBody:{
+            actionResponse: {},
+            annotations: [],
+            argumentText: "my_argumentText",
+            attachment: [],
+            cards: [],
+            createTime: "my_createTime",
+            fallbackText: "my_fallbackText",
+            // lastUpdateTime: "my_lastUpdateTime",
+            // name: "my_name",
+            previewText: "my_previewText",
+            sender: {},
+            slashCommand: {},
+            space: {
+                name: 'spaces/AAAASGYz1ug',
+                type: 'ROOM',
+                singleUserBotDm: false,
+                threaded: true,
+                displayName: 'Pruebas alertas'
+              },
+            text: message,
+            thread: {}
+        }
+    }).catch(e => {logger.error(e); throw e});
+    logger.info(res);
+    return res;
+}
   
-  async function getMessage2(space, message, thread){
-  const auth = new google.auth.GoogleAuth({
-      // Scopes can be specified either as an array or as a single, space-delimited string.
-      scopes: ['https://www.googleapis.com/auth/chat.bot'],
+async function getMessage(space, message, thread){
+    google.options({auth: _auth});
+    const res = await gchat.spaces.messages.get({
+        name: `spaces/${space}/messages/${thread}.${message}`,
     });
-    const authClient = await auth.getClient();
-    google.options({auth: authClient});
-  const res = await gchat.spaces.messages.get({
-      name: `spaces/${space}/messages/${thread}.${message}`,
-  });
-  console.log(res.data);
-  };
-  
-  function getMessage(space, message) {
-    return new Promise(function(resolve, reject) {
-        getJWT().then(function(token) {
-            unirest.get(`https://chat.googleapis.com/v1/spaces/${space}/messages/${message}.${message}`)
-                .headers({
-                    "Content-Type": "application/json",
-                    "Authorization": "Bearer " + token
-                })
-   //             .send(JSON.stringify())
-                .end(function(res) {
-                        console.log(res);
-                    resolve();
-                });
-        }).catch(function(err) {
-            reject(err);
-        });
-    });
-  }
-  
-  const webhookURL = '';
-  const apiURL = '';
-  app.post('/', (req, res) => {
-      console.log(req.body);
-    res.send('Hello World!')
-  })
-  
-  app.get('/update/:space/:id/:thread', (req, res) => {
-      updateMessage(req.params.space, req.params.thread, req.params.id) 
-      .then(()=>{
-          res.send("Updated");
-      }).catch((e)=>{
-          console.log(e);
-              res.send("Error");
-      });
-  
-      res.send("Updated exit");
-  });
-  
-  app.get('/get/:space/:id/:thread', (req, res) => {
-      getMessage2(req.params.space, req.params.id, req.params.thread).then(()=>{
-  
-      res.send("Updated exit");
-      }).catch((e)=>{res.send("Error:");console.log(e);});
-      
-  });
+    return res;
+};
+
+module.exports.updateMessage = updateMessage;
+module.exports.sendMessage = sendMessage;
+module.exports.getMessage = getMessage;
--- src/routes/hook.js
+++ src/routes/hook.js
@@ -1,17 +1,26 @@
 // hook.js - Route that handles AlertManager calls
-
-var express = require('express');
 const logger = require('../helpers/logger');
+
+var gchat = require('../helpers/googleChatApi.js');
+var express = require('express');
 var router = express.Router();
 
 router.use(express.json());
 
-// Home page route.
-router.get('/', function (req, res) {
-    res.sendStatus(200);
+
+// Status
+router.get('/', function (req, res) {   
+    gchat.sendMessage(process.env.TEST_THREAD, null, null, "prueba")
+    .then(() => {
+        res.sendStatus(200);
+    })
+    .catch((error) => {
+        logger.error(error);
+        res.sendStatus(500);
+    });
 })
 
-// About page route.
+// Main Entrypoint for Alerts
 router.post('/', function (req, res) {
     logger.info("Response", req.body.alerts[0]);
     res.send('Hook post');
