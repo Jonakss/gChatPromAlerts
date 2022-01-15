5f839e5df63a6a99570f19687910251b732c9e8a
Jonathan Correa Paiva <jona.correapc@gmail.com>
2022-01-15 14:27:28 -0300

    Added fingerprints/threads objects

--- src/routes/hook.js
+++ src/routes/hook.js
@@ -1,5 +1,16 @@
 // hook.js - Route that handles AlertManager calls
 
+var fingerprints = []; // fingerprints[fingerprintId]
+
+function addFingerprint(fingerprintId, thread){
+    fingerprints[fingerprintId] = thread;
+}
+
+function getThread(fingerprintId){
+    if (fingerprints_ === undefined || Object.keys(fingerprints).length === 0) return undefined;
+    return fingerprints[fingerprintId];
+};
+
 var express = require('express');
 const logger = require('../helpers/logger');
 var router = express.Router();
@@ -15,12 +26,13 @@ router.get('/', function (req, res) {
 router.post('/', function (req, res) {
     //logger.info("Body", req.body[0]);
     logger.info("Alert conts", req.body.alerts.length);
-    if (req.body.alerts.length > 0){
-
+    if (req.body.alerts.length >= 0){
         req.body.alerts.map((a, i)=>{
             logger.info(`Alert ${i}: `, a.fingerprint);
+            addFingerprint(a.fingerprint, "Thread id")
         })
     };
+    if (fingerprints_ !== undefined || Object.keys(fingerprints).length >= 0) fingerprints.map(f => console.log(f));
     res.send('Hook post');
 })
 
